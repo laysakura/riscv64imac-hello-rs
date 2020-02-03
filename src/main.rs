ce1273eafc56e47bfa3c2b555d8d5d7d4ce3356d
Sho Nakatani <lay.sakura@gmail.com>
2020-02-03 13:42:39 +0900

    a bit more comment on main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,26 @@
 
 #[no_mangle]
 pub extern "C" fn __start_rust() -> ! {
-    let uart = 0x1000_0000  as *mut u8;
+    // See: https://gitlab.freedesktop.org/spice/qemu/blob/e24f44dbeab8e54c72bdaedbd35453fb2a6c38da/hw/riscv/virt.c#L57
+    let uart = 0x1000_0000 as *mut u8;
+
     for c in b"Hello from Rust!".iter() {
         unsafe {
             *uart = *c as u8;
         }
     }
 
-    loop{}
+    loop {}
 }
 
 use core::panic::PanicInfo;
 #[panic_handler]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn abort() -> ! {
-    loop{}
+    loop {}
 }
